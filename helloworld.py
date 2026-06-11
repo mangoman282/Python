2fe13381d98086ef5496f440a5b6f763223b26d4
TuanDung <dungtuanxz12345@gmail.com>
2026-06-11 09:19:48 +0700

    Introduction

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+#introduction to python
+print("Hello World") #printing Hello World 
+
+print(3+2) #addition
+print(3-2) #subtraction
+print(3*2) #multiplication
+print(3/2) #division
+print(3//2) #floor division
+print(3%2) #modulus
+print(3**2) #exponentiation
+
+#checking data types
+print(type(3)) #integer
+print(type(3.0)) #float 
+print(type("Hello World")) #string
+print(type(True)) #boolean
+print(type(None)) #NoneType
+print(type(1+3j)) #complex number
+print(type([1,2,3])) #list
+print(type((1,2,3))) #tuple
+print(type({1: "one", 2: "two", 3: "three"})) #dictionary
+print(type({1,2,3})) #set
